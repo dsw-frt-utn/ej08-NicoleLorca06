ddd1574d8cd5acedcafa4c67f1a96a34c80aea26
Lorca Karen Nicole <karennicolelorca06gmail.com>
2026-05-01 15:59:31 -0300

    Problema 2 completado: Implementacion de CrearResumenVentas

---
+++ Ejercicio8/Ejercicio8/Problema2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal untilPrice)
+        {
+            decimal TotalCalculado = quantity > 0 ? quantity * untilPrice : 0;
+
+            var resumen = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = TotalCalculado,
+            };
+
+            return $"{resumen.Code}-{resumen.Description}-{resumen.Total}";
+        }
+    }
+    
+}
--- Ejercicio8/Ejercicio8/Program.cs
+++ Ejercicio8/Ejercicio8/Program.cs
@@ -12,8 +12,12 @@
             Console.WriteLine(etiqueta);
             Console.WriteLine(etiqueta2);
 
+            Console.WriteLine("--------MONTO TOTAL--------");
 
+            Problema2 p2 = new Problema2();
 
+            string resultado = p2.CrearResumenVenta(101, "Teclado Logitech", 2, 45000.00m);
+            Console.WriteLine(resultado);
         }
     }
 }
