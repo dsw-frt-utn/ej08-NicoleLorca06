d6fd88b24f4832a6a19df868914ca416fa213036
Lorca Karen Nicole <karennicolelorca06gmail.com>
2026-05-01 17:18:09 -0300

    Problema 4 completado: Implementacion de CalcularPromedio

---
+++ Ejercicio8/Ejercicio8/Problema4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio8
+{
+    internal class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            double suma = 0;
+            int Contador = 0;
+
+            int n1 = nota1 ?? -1;
+            if (n1 >= 0 && n1 <=10)
+            {
+                suma += n1;
+                Contador++;
+            }
+
+            int n2 = nota2 ?? -1;
+            if (n2 >= 0 && n2 <= 10)
+            {
+                suma += n2;
+                Contador++;
+            }
+
+            int n3 = nota3 ?? -1;
+            if (n3 >= 0 && n3 <= 10)
+            {
+                suma += n3;
+                Contador++;
+            }
+
+            return Contador == 0 ? 0 : suma / Contador;
+
+        }
+
+    }
+}
--- Ejercicio8/Ejercicio8/Program.cs
+++ Ejercicio8/Ejercicio8/Program.cs
@@ -13,6 +13,7 @@ namespace Ejercicio8
 
             Console.WriteLine(etiqueta);
             Console.WriteLine(etiqueta2);
+            Console.WriteLine("-----------------------------------------");
 
             Console.WriteLine("--------MONTO TOTAL: PTO2--------");
 
@@ -20,6 +21,7 @@ namespace Ejercicio8
 
             string resultado = p2.CrearResumenVenta(101, "Teclado Logitech", 2, 45000.00m);
             Console.WriteLine(resultado);
+            Console.WriteLine("-----------------------------------------");
 
 
             Console.WriteLine("--------COPIAS: PTO3--------");
@@ -30,7 +32,28 @@ namespace Ejercicio8
             miProducto.SetDescripcion("Original");
 
             Console.WriteLine(p3.CompararCopias(104, miProducto));
-            
+
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("---------- PRUEBAS PROBLEMA 04 ----------");
+
+            var p4 = new Problema4();
+
+            int? v1 = 8, v2 = 7, v3 = 9;
+
+            double promedioValido = p4.CalcularPromedio(v1, v2, v3);
+
+            Console.WriteLine($"Prueba valida (8, 7, 9): {promedioValido}");
+
+            int? n1 = 10, n2 = null, n3 = -2;
+
+            double promedioNull = p4.CalcularPromedio(n1, n2, n3);
+            Console.WriteLine($"Prueba invalida (10, null, -2): {promedioNull}");
+
+            double promedioTodoNull = p4.CalcularPromedio(null, null, null);
+            Console.WriteLine($"Prueba Todo Null: {promedioTodoNull}");
+
+            Console.WriteLine("-----------------------------------------");
 
 
         }
